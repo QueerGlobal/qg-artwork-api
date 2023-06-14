84ed97f3c77c12942f24d10cb6bf791de4548ee5
Christopher Watson <christopherwatson@Christophers-MacBook-Pro.local>
2023-06-14 08:10:24 -0400

    update artwork api to add uri, setup docker-compose

--- scripts/mock_api_start.sh
+++ scripts/mock_api_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # start mock docker containers
-docker run --name mock-artwork-api -d -p 3001:3001 mock-artwork-api &
+docker run --name mock-artwork-api -d -p 3001:31001 mock-artwork-api &
