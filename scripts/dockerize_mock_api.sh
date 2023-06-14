4c8048ee62df8c781ac1a3da0de641ad1b6dfefa
Christopher Watson <christopherwatson@Christophers-MacBook-Pro.local>
2023-06-14 08:19:07 -0400

    update start, add stop script

--- scripts/dockerize_mock_api.sh
+++ scripts/dockerize_mock_api.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mockoon-cli dockerize --data api/testing/mocks/artwork.json --port 31001  --output api/testing/mocks/artwork_dockerfile
+mockoon-cli dockerize --data api/testing/mocks/mockoon-mocks.json --port 31001  --output api/testing/mocks/artwork_dockerfile
